88d252fd16ac8727f2ca9c74bfbdd509af5472e0
mnichangxin <mnichangxin@163.com>
2018-12-06 22:52:51 +0800

    addd auth

---
+++ flaskr/auth.py
@@ -0,0 +1,26 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+        elif db.execute (
+            'SELECT id FROM user WHERE username = ?', (username)
+        ).fetchone() is not None:
+            error = 'User {} is already register.'.format(username)
