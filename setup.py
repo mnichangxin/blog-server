8fc634d18ef2355547d0e271583ffc387e360850
mnichangxin <mnichangxin@163.com>
2018-12-13 10:58:08 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import find_packages, setup
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name = 'flaskr',
     version = '0.0.1',
+    author = 'mnichangxin',
+    author_email = 'mnichangxin@163.com',
+    url = 'https://github.com/mnichangxin/vue-blog-server',
+    description = 'Flask server',
     packages = find_packages(),
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
     include_package_data = True,
     zip_safe = False,
     install_requires = [
-        'flask',
+        'flask'
+    ],
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ]
 )
