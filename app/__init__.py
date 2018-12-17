8544629a9d4c7a766a97be5d2083b02292c03310
mnichangxin <mnichangxin@163.com>
2018-12-17 16:54:07 +0800

    upadte app

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from config import config
 
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
     from .api import api as api_blueprint
-    app.register_blueprint(api_blueprint, url_prefix = '/api/v1')
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix = '/auth')
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
