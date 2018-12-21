91f5e186738c1aa7c9c58813de805ec314870d74
mnichangxin <mnichangxin@163.com>
2018-12-21 19:24:22 +0800

    add flask-sqlalchemy

--- config.py
+++ config.py
@@ -2,17 +2,21 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class BaseConfig:
     SECRET_KEY = 'dev'
     DATABASE = os.path.join(basedir, 'instance', 'flaskr.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
 
     @staticmethod
     def init_app():
         pass
 
+
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    
+
+
 class TestingConfig(BaseConfig):
     TESTING = True
 
--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 from setuptools import find_packages, setup
 
-with open('README.md', 'r', encoding = 'UTF-8') as fh:
+with open('README.md', 'r', encoding='UTF-8') as fh:
     long_description = fh.read()
 
 setup(
-    name = 'flaskr',
-    version = '0.0.1',
-    author = 'mnichangxin',
-    author_email = 'mnichangxin@163.com',
-    url = 'https://github.com/mnichangxin/vue-blog-server',
-    description = 'Flask server',
-    packages = find_packages(),
-    long_description = long_description,
-    long_description_content_type = 'text/markdown',
-    include_package_data = True,
-    zip_safe = False,
-    install_requires = [
-        'flask'
+    name='flaskr',
+    version='0.0.1',
+    author='mnichangxin',
+    author_email='mnichangxin@163.com',
+    url='https://github.com/mnichangxin/vue-blog-server',
+    description='Flask server',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'flask-sqlalchemy'
     ],
-    classifiers = [
+    classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
