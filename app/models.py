91f0e0b77222d5c153a5feb5fdd5c53cba83e178
mnichangxin <mnichangxin@163.com>
2019-01-02 17:01:43 +0800

    update models

--- app/models.py
+++ app/models.py
@@ -1,4 +1,22 @@
-class Article:
-    __tablename__ = 'blog_article'
-    id = Column(Integer, primary_key=True)
-    title = Column(String(length=30))
+from datetime import datetime
+from . import db
+
+
+class Article(db.Model):
+    __tablename__ = 'articles'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(length=30))
+    date = db.Column(db.Datetime, default=datetime.utcnow)
+    category = db.Column()
+
+    def __repr__(self):
+        return '<Article %r>' % self.title
+
+
+class category(db.Model):
+    __tablename__ = 'categories'
+    article_id = db.Column(db.Integer)
+    category_name = db.Column(db.String(length=30))
+
+    def __repr__():
+        return '<Category %r>' % self.category_name
