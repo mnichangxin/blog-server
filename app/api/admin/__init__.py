a546beeea71c3a9ee9f294cff10e6b7370866288
mnichangxin <mnichangxin@163.com>
2019-05-22 23:59:23 +0800

    update api

--- app/api/admin/__init__.py
+++ app/api/admin/__init__.py
@@ -6,13 +6,13 @@ from ...db import Post
 
 admin = Blueprint('admin', __name__)
 
-@admin.route('/login', methods=['POST'])
-def login():
-    return 'admin login.'
+# @admin.route('/login', methods=['POST'])
+# def login():
+#     return 'admin login.'
 
-@admin.route('/logout', methods=['POST'])
-def logout():
-    return 'admin logout.'
+# @admin.route('/logout', methods=['POST'])
+# def logout():
+#     return 'admin logout.'
 
 @admin.route('/publish', methods=['POST'])
 @resp_wrapper
