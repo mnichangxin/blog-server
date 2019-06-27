0e88b35fb91ec10e61116fe4e3c52d100cb55b08
mnichangxin <mnichangxin@163.com>
2019-06-28 01:41:03 +0800

    feature:publish post

--- server/service/admin/common.py
+++ server/service/admin/common.py
@@ -1,14 +1,22 @@
 from flask import jsonify
 from datetime import datetime
 from ...dao.post import Post
-from ...utils.exception.exception import APIException
+from ...utils.exception.exception import APIException, ServerException
 from ...utils.decorators.is_json import is_json
 
 
 @is_json
 def post_publish(params):
-    content = params.get('cotent')
-    first_date = params.get('first_date')
-    if content is None or first_date is None:
-        raise APIException(u'参数错误', 400)
-    Post.insert()
+    title = params.get('title')
+    content = params.get('content') or ''
+    created_date = params.get('created_date')
+    updated_date = params.get('updated_date') or datetime.utcnow
+    if title is None or created_date is None:
+        raise APIException('参数错误', 400)
+    try:
+        Post.insert(**params)
+    except Exception as e:
+        raise ServerException('服务端错误', 500)
+    return {
+        'msg': '发布成功'
+    }
