695e9c815de3d317e7e31eb4f94bae4bc8066821
mnichangxin <mnichangxin@163.com>
2019-06-28 01:41:56 +0800

    feature:dao function

--- server/dao/post.py
+++ server/dao/post.py
@@ -1,17 +1,11 @@
-from flask import jsonify
-from datetime import datetime
-from ..utils.exception.exception import APIException
 from ..model.common import db
 from ..model.post import PostModel
 
 
 class Post:
-    def insert(self, *arg, **kwargs):
-        post = PostModel(arg)
+    @staticmethod
+    def insert(**kwargs):
+        post = PostModel(**kwargs)
         db.session.add(post)
         db.session.commit()
-        return ''
-    def get(self, offset=0, limit=10):
-        if not (limit and limit <= 50):
-            raise APIException(u'limit must between 0 and 50', 400)
-        return PostModel.query.all()
+
