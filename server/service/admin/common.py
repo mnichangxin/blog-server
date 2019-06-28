1cb8393b648774dce54efd597143dac7c557c4f1
mnichangxin <mnichangxin@163.com>
2019-06-29 01:44:23 +0800

    update api

--- server/service/admin/common.py
+++ server/service/admin/common.py
@@ -9,14 +9,24 @@ from ...utils.decorators.is_json import is_json
 def post_publish(params):
     title = params.get('title')
     content = params.get('content') or ''
-    created_date = params.get('created_date')
-    updated_date = params.get('updated_date') or datetime.utcnow
-    if title is None or created_date is None:
+    created_date = params.get('created_date') or datetime.now()
+    updated_date = None
+    if title is None:
         raise APIException('参数错误', 400)
     try:
-        Post.insert(**params)
-    except Exception as e:
+        datetime.strptime(created_date, '%Y-%m-%d %X')
+    except ValueError:
+        raise APIException('不合法的创建时间', 400)
+    try:
+        Post.insert(title=title, content=content, created_date=created_date, updated_date=updated_date)
+    except Exception:
         raise ServerException('服务端错误', 500)
     return {
         'msg': '发布成功'
     }
+
+# @is_json
+# def post_update(params):
+#     title = params.get('title')
+#     content = params.get('content')
+#     created_date = params.get('created_date')
