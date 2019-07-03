786e3630d1213ccfbd43874746629741f380c0ec
mnichangxin <mnichangxin@163.com>
2019-07-04 01:01:47 +0800

    add category dao

---
+++ server/dao/category.py
@@ -0,0 +1,14 @@
+from ..model.common import db
+from ..model.category import CategoryModel
+
+class Category:
+    @staticmethod
+    def insert(**kwargs):
+        db.session.add(CategoryModel(**kwargs))
+        db.session.commit()
+    @staticmethod
+    def queryById(id):
+        return CategoryModel.query.filter_by(id=id).first()
+    @staticmethod
+    def queryByCategoryName(category_name):
+        return CategoryModel.query.filter_by(category_name=category_name).first()
--- server/service/admin/common.py
+++ server/service/admin/common.py
@@ -1,25 +1,34 @@
 from flask import jsonify
 from datetime import datetime
 from ...dao.post import Post
+from ...dao.category import Category
 from ...utils.exception.exception import APIException, ServerException
 from ...utils.decorators.is_json import is_json
 
-import traceback
 
 @is_json
 def post_publish(params):
     title = params.get('title')
     content = params.get('content') or ''
+    category = params.get('category')
     created_date = params.get('created_date') or datetime.now()
     updated_date = None
     if title is None:
         raise APIException('参数错误', 400)
+    if category is not None:
+        if Category.queryByCategoryName(category) is not None:
+            
     try:
         datetime.strptime(created_date.strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
     except (AttributeError, ValueError):
         raise APIException('不合法的创建时间', 400)
     try:
-        Post.insert(title=title, content=content, created_date=created_date, updated_date=updated_date)
+        Post.insert(
+            title=title, 
+            content=content, 
+            created_date=created_date, 
+            updated_date=updated_date
+        )
     except Exception:
         raise ServerException('服务端错误', 500)
     return {
