cc230f750ce39a0ef2f1322e331a9c7ecd6a1703
mnichangxin <mnichangxin@163.com>
2019-07-11 01:47:22 +0800

    update category model

--- server/model/category.py
+++ server/model/category.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Integer, String, Column
-from . import db
+from .common import db
 
 class CategoryModel(db.Model):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
-    category_name = Column(String(30), unique=True, nullable=False)
+    category_name = Column(String(30), nullable=False)
 
     def __repr__(self):
         return '<Category %r>' % self.category_name
