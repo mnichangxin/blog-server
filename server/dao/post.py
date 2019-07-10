1b64e36440cfcb8dcf82a6625a87510f906f4edd
mnichangxin <mnichangxin@163.com>
2019-07-11 01:47:40 +0800

    update post dao

--- server/dao/post.py
+++ server/dao/post.py
@@ -1,11 +1,11 @@
 from ..model.common import db
 from ..model.post import PostModel
-
+from ..utils.decorators.commit import commit
 
 class Post:
     @staticmethod
+    @commit
     def insert(**kwargs):
         post = PostModel(**kwargs)
         db.session.add(post)
-        db.session.commit()
 
