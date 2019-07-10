e308ac36088b5f85d6794418f26e3f42ebaa1965
mnichangxin <mnichangxin@163.com>
2019-07-11 01:46:26 +0800

    update service

--- server/service/admin/common.py
+++ server/service/admin/common.py
@@ -4,23 +4,25 @@ from ...dao.post import Post
 from ...dao.category import Category
 from ...utils.exception.exception import APIException, ServerException
 from ...utils.decorators.is_json import is_json
+from ...utils.decorators.commit import commit
 
-
+@commit
 @is_json
 def post_publish(params):
     title = params.get('title')
     content = params.get('content') or ''
-    category = params.get('category')
+    category_name = params.get('category_name')
     created_date = params.get('created_date') or datetime.now()
     updated_date = None
     if title is None:
         raise APIException('参数错误', 400)
-    if category is not None:
-        if Category.queryByCategoryName(category) is not None:
-            Category.insert(category_name=category)
+    if category_name is not None:
+        category_name_query = Category.queryByCategoryName(category_name)
+        if category_name_query is not None:
+            pass
         else:
-            raise
-    try:
+            Category.insert(category_name=category_name)
+    try: 
         datetime.strptime(created_date.strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
     except (AttributeError, ValueError):
         raise APIException('不合法的创建时间', 400)
