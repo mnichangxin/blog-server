1c09f32dc9c53b7eb5a5bf85c8fecb8b836dcdec
mnichangxin <mnichangxin@163.com>
2019-07-21 22:37:17 +0800

    :sparkles: Add delete api

--- server/api/internal/common.py
+++ server/api/internal/common.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request
 from ...utils.decorators.res_wrapper import resp_wrapper
-from ...service.common import post_publish, post_query
+from ...service.common import post_publish, post_query, post_delete
 
 internal = Blueprint('internal', __name__)
 
@@ -13,3 +13,8 @@ def api_post_publish():
 @resp_wrapper
 def api_post_query():
     return post_query(request.get_json())
+
+@internal.route('/post_delete', methods=['POST'])
+@resp_wrapper
+def api_post_delete():
+    return post_delete(request.get_json())
