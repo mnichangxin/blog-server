3c5356db1d77611bf3b50f739d0343e608318df5
mnichangxin <mnichangxin@163.com>
2019-07-21 14:52:35 +0800

    :sparkles: Add model object to dict

--- server/model/tb.base.py
+++
@@ -1,8 +0,0 @@
-from .common import db
-
-class Base(db.Model):
-    def to_dict(self):
-        return { column.name: getattr(self, column.name, None) for column in self.__table__.columns }
-    
-    def __repr__(self):
-        return '<Base>'
