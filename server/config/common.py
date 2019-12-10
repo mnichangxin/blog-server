c4f4c3e3f4525dce851586dd991655adbbefdeba
mnichangxin <mnichangxin@163.com>
2019-12-10 15:43:49 +0800

    :wrench: Change db config

--- server/config/common.py
+++ server/config/common.py
@@ -1,8 +1,15 @@
+'''
+    Notice: The database config is confidential information, so not open source code. You can create owner db config If you need. 
+'''
+from .db_secure import config as db_config
+
 class BaseConfig:
     '''Base config'''
     SECRET_KEY = 'dev'
-    SQLALCHEMY_DATABASE_URI = '''mysql+mysqlconnector://root:12345678@localhost:3306
-                                                    /blog?charset=utf8'''
+    SQLALCHEMY_DATABASE_URI = '''mysql+mysqlconnector://{username}:{password}@{host}/{database}?charset={charset}''' \
+                                .format(host=db_config['HOST'], port=db_config['PORT'], \
+                                username=db_config['USERNAME'], password=db_config['PASSWORD'], \
+                                database=db_config['DATABASE'], charset=db_config['CHARSET'])
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
