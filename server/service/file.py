c56c789b15fa80d80737486bace0a9985cff34df
mnichangxin <mnichangxin@163.com>
2019-12-21 23:48:29 +0800

    Add file convert logic

--- server/service/file.py
+++ server/service/file.py
@@ -1,16 +1,22 @@
+from markdown import markdown
 from server.utils.exception.exception import APIException, ServerException
 from server.utils.decorators.json_required import json_required
 from server.utils.decorators.commit import commit
+
+# md = Markdown(extensions=['meta'], output_format='html5')
 
 @commit
 def fileUpload(files):
     filesCount = len(files)
     if filesCount == 0:
         raise APIException('文件不能为空')
-    for i in files:
-        file = files[i]
-        if file.content_type != 'text/markdown':
-            raise APIException('文件格式不正确')
+    file = files['file']
+    if file is None:
+        raise APIException('file参数不能为空')
+    if file.content_type != 'text/markdown':
+        raise APIException('文件格式不正确')
+    html_content = markdown(file.read().decode(encoding='utf-8'))
     return {
-        'msg': '上传成功'
+        'msg': '上传成功',
+        'data': html_content
     }
