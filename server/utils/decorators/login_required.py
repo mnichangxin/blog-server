bf202cc182b9b2c653eb0f989f63c28476372234
mnichangxin <mnichangxin@163.com>
2019-12-26 14:27:03 +0800

    :sparkles: Add login_required decorator

--- server/utils/decorators/login_required.py
+++ server/utils/decorators/login_required.py
@@ -1,9 +1,13 @@
 from functools import wraps
 from flask import request
 from server.utils.exception.exception import APIException, ServerException
+from server.service.user import checkLoginStatus
 
 def login_required(func):
     @wraps(func)
     def decorator(*args, **kwargs):
+        cookies = request.cookies
+        if not checkLoginStatus(cookies):
+            raise APIException('用户未登录', 403)
         return func(*args, **kwargs)
     return decorator
