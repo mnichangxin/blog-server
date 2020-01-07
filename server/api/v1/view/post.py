2100a3dbac6b817a07f2a51debbc8f1e42d8294f
mnichangxin <mnichangxin@163.com>
2020-01-07 22:21:45 +0800

    Optimazing code

--- server/api/v1/view/post.py
+++ server/api/v1/view/post.py
@@ -5,38 +5,11 @@ from server.utils.decorators.json_required import json_required
 from server.service.post import postPublish, postQuery, postDelete, postUpdate
 from server.service.file import fileUpload
 
-bp = Blueprint('posts', __name__, url_prefix='/v1/view/post')
 
-@bp.route('/publish', methods=['POST'])
-@resp_wrapper
-@login_required
-@json_required
-def post_publish():
-    return postPublish(request.get_json())
+bp = Blueprint('posts', __name__, url_prefix='/v1/view/post')
 
 @bp.route('/query', methods=['GET'])
 @resp_wrapper
-@login_required
 @json_required
 def post_query():
     return postQuery(request.get_json())
-
-@bp.route('/delete', methods=['POST'])
-@resp_wrapper
-@login_required
-@json_required
-def post_delete():
-    return postDelete(request.get_json())
-
-@bp.route('/update', methods=['POST'])
-@resp_wrapper
-@login_required
-@json_required
-def post_update():
-    return postUpdate(request.get_json())
-
-@bp.route('/fileUpload', methods=['POST'])
-@resp_wrapper
-@login_required
-def file_upload():
-    return fileUpload(request.files)
